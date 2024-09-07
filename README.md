f3ea475afecd44d25ed8c9b085b81e6c24fbd8df
josoaaa <joaosoaresjosoa@gmail.com>
2024-09-07 19:15:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS2-RoundEndKill
+ A simple plugin that kill everyone when round end.
---
+++ RoundEndKill.cs
@@ -0,0 +1,29 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+
+namespace RoundEndKill;
+
+public class RoundEndKill : BasePlugin
+{
+    public override string ModuleName => "RoundEndKill";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "Josoa";
+    public override string ModuleDescription => "A simple plugin that kill everyone when round end";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterEventHandler<EventRoundEnd>(OnRoundEnd);
+    }
+
+    private HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
+    {   
+        var players = Utilities.GetPlayers();
+        foreach (var player in players)
+        {
+            player.CommitSuicide(false, true);
+        }
+
+        return HookResult.Continue;
+    }
+}
